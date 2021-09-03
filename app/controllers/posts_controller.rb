d0fde0f30a4dd4234082333c7d01e9fa9e0b8bcb
sherlok6 <skibakol@yandex.ru>
2021-09-03 19:53:19 +0300

    Create Model Post and CRUD interface

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,32 @@
 class PostsController < ApplicationController
     def index
+        @posts = Post.all
+    end
+    def new
+    end
+    def create
+        @post = Post.new(post_params)
+        @post.dt_post = DateTime.current
+        @post.save
+        redirect_to @post
+    end
+    def show
+        @post = Post.find(params[:id])
+    end
+    def edit
+        @post = Post.find(params[:id])
+    end
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        redirect_to @post
+    end
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+    private def post_params
+        params.require(:post).permit(:title,:body)
     end
 end
