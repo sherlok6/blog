e4f15722be03d2dfa8f61ac8122e9526b6cf9a17
sherlok6 <skibakol@yandex.ru>
2021-09-04 22:25:11 +0300

    Add model Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_180519) do
+ActiveRecord::Schema.define(version: 2021_09_04_192341) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "username"
+    t.text "body"
+    t.datetime "times"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
